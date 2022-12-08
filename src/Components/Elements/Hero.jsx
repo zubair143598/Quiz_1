950f991dfa2f8bb88801fa76fa8aff2df96a9af0
M Zubair <md.zubair143598@gmail.com>
2022-12-08 18:04:18 +0500

    init

---
+++ src/Components/Elements/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Hero = () => {
+  const Quiz=
+  [
+    {
+      question:""
+    }
+  ]
+  return (
+    <div className="hero-section">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12 text-center mt-5">
+            <h1>Quiz</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
